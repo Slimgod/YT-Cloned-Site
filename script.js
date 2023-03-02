d86f599c4bffa10c7c1c88802833bf53c15090e5
Victor <111026029+Slimgod@users.noreply.github.com>
2023-03-02 02:11:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-var menuIcon =document.querySelector(".menu-icon");
+var  menuIcon =document.querySelector(".menu-icon");
 var sidebar =document.querySelector(".sidebar");
 var largecontainer =document.querySelector(".container");
 
+    const writeNewcommnt= document.getElementById("writecomments").value;
+    // let displaycomments=document.getSelector(".displaycomments");
+
+let reaction = 0;
+let reactLike=document.querySelector(".likes");
+let reactdislike=document.querySelector(".dislikes");
 menuIcon.onclick= function(){
     sidebar.classList.toggle("small-sidebar");
-    largecontainer.classList.toggle("largecontainer")
+    largecontainer.classList.toggle("largecontainer");
 };
+
+function react(){
+    reaction=+1;
+   return reactLike.innerHTML=reaction;
+}
+function reacts(){
+    reaction=+1;
+    return reactdislike.innerHTML=reaction;
+ 
+  
+}
+function submit(){
+    // writeNewcommnt.innerHTML=displaycomments;
+    // return console.log(writeNewcommnt);
+    console.log(writeNewcommnt)
+};
+submit()
+ 
+ 
